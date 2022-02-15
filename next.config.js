601581736616356378488a4309351eba7a471efa
abhi-sriram <abhisriram007@gmail.com>
2022-02-15 10:55:01 +0530

    Merge branch 'master' of https://github.com/abhi-sriram/sgc

--- next.config.js
+++ next.config.js
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-module.exports = {
-  reactStrictMode: true,
-}
+/** @type {import('next-pwa').withPWA} */
+
+
+
+const withPWA = require('next-pwa')
+ 
+module.exports = withPWA({
+    pwa: {
+        dest: 'public',
+        register: true,
+        skipWaiting: true,
+        // disable: process.env.NODE_ENV === 'development',
+    },
+    reactStrictMode: true,
+})
